3cfcd4502e31a9c48d18724acc1d89cd2c2ff3ea
Shavinda Silva <shavinda34@gmail.com>
2022-07-03 19:59:29 +0930

    Extract vendor files to public directory to increase performance

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,9 @@
   <script src="https://cdn.tailwindcss.com"></script>
   <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
   <script src="{{ mix('/js/app.js') }}" defer></script>
+  <script src="{{ mix('/js/vendor.js') }}" defer></script>
+  <script src="{{ mix('/js/manifest.js') }}" defer></script>
+  
   @inertiaHead
 </head>
 
